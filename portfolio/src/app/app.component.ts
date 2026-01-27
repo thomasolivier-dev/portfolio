4d1e6893b29361e8a29ad7d5c08f050d20353433
thomasolivier-dev <thomas.olivier.pro25@gmail.com>
2026-01-27 15:36:52 +0100

    add base of navbar

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './core/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
