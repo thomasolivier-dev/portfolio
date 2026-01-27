ea4875f0de3bd6c20079c18b7e8f3618fed96f07
thomasolivier-dev <thomas.olivier.pro25@gmail.com>
2026-01-27 22:09:02 +0100

    add nav menu for mobile and animation

--- portfolio/src/app/core/navbar/navbar.component.ts
+++ portfolio/src/app/core/navbar/navbar.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { MenuItem } from '../../shared/models/menu-items.model';
+import { MenuAnimationsService } from '../../services/animations/menu-animations.service';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  animations: [
+    MenuAnimationsService.slideFromright
+  ]
 })
 export class NavbarComponent {
+  isOpenMenu: boolean = false;
 
+  toggleMenu(): void {
+    this.isOpenMenu=!this.isOpenMenu;
+  }
+
+  closeMenu(): void {
+    this.isOpenMenu=false;
+  }
 
   readonly menuItems: MenuItem[] = [
       { label: 'À propos', link: '#about' },
