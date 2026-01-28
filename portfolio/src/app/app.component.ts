16021e833c5812cf5950697c11ce06056a5bafca
thomasolivier-dev <thomas.olivier.pro25@gmail.com>
2026-01-28 13:05:36 +0100

    feat: add copyrigth

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './core/navbar/navbar.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
