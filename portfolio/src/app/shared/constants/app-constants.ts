759ad4543a28701227d9aa98352560a37a35249e
thomasolivier-dev <thomas.olivier.pro25@gmail.com>
2026-01-28 23:54:10 +0100

    feat: add icon link

---
+++ portfolio/src/app/shared/constants/app-constants.ts
@@ -0,0 +1,8 @@
+import { MenuItem } from '../models/menu-items.model';
+
+export const menuItems: MenuItem[] = [
+        { label: 'À propos', link: '#about' },
+        { label: 'Compétences', link: '#skills' },
+        { label: 'Projets', link: '#projects' },
+        { label: 'Contact', link: '#contact' }
+      ]; 
