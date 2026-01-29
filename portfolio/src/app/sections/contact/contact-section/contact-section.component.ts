4f4f849efbd7d38e4b22f19df794da122b42bb9b
thomasolivier-dev <thomas.olivier.pro25@gmail.com>
2026-01-29 16:44:40 +0100

    add: section contact design

---
+++ portfolio/src/app/sections/contact/contact-section/contact-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
+
+@Component({
+  selector: 'app-contact-section',
+  imports: [ContactFormComponent],
+  templateUrl: './contact-section.component.html',
+  styleUrl: './contact-section.component.scss'
+})
+export class ContactSectionComponent {
+
+}
