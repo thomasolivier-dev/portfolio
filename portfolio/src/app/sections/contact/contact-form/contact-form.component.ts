efea8c363602a3c5a960fa6906d993cca20fc43b
thomasolivier-dev <thomas.olivier.pro25@gmail.com>
2026-01-30 00:03:13 +0100

    feat: add form validation and react form logic

--- portfolio/src/app/sections/contact/contact-form/contact-form.component.ts
+++ portfolio/src/app/sections/contact/contact-form/contact-form.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
+
+  readonly contactForm : FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    subject: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    message: new FormControl('', [Validators.required])
+  })
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      this.contactForm.reset()
+    } else {
+      this.contactForm.markAllAsTouched()
+    }
+  }
 
 }
