d95fe462aa40004fed532d6013b9be280d05a1ab
thomasolivier-dev <thomas.olivier.pro25@gmail.com>
2026-02-02 01:27:00 +0100

    feat: add section techno in project + add new techno service and refactor project service

--- portfolio/src/app/sections/project/project-card/project-card.component.ts
+++ portfolio/src/app/sections/project/project-card/project-card.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { ImageTooltipComponent } from '../../../shared/components/tooltips/image-tooltip/image-tooltip.component';
+import { Techno } from '../../../shared/models/techno.model';
 
 @Component({
   selector: 'app-project-card',
-  imports: [],
+  imports: [ImageTooltipComponent],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
@@ -12,5 +14,6 @@ export class ProjectCardComponent {
   @Input() desc!: string;
   @Input() githubUrl?: string;
   @Input() siteUrl?: string;
+  @Input() listTechno?: Techno[];
 
 }
--- portfolio/src/app/shared/models/projects.model.ts
+++ portfolio/src/app/shared/models/projects.model.ts
@@ -1,7 +1,19 @@
+import { Techno } from "./techno.model";
+
+export interface ProjectRaw {
+  title: string;
+  desc: string;
+  githubUrl?: string;
+  siteUrl?: string;
+  image: string;
+  technos: string[];
+}
+
 export interface Project {
   title: string;
   desc: string;
-  githubUrl: string;
-  siteUrl: string;
+  githubUrl?: string;
+  siteUrl?: string;
   image: string;
+  technos?: Techno[];
 }
