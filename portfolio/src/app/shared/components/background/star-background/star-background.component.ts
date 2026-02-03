08243cb8355b515362aee3a26473891c479f481f
thomasolivier-dev <thomas.olivier.pro25@gmail.com>
2026-02-03 23:50:25 +0100

    feat: add star background + fix position of element

---
+++ portfolio/src/app/shared/components/background/star-background/star-background.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-background',
+  imports: [],
+  templateUrl: './star-background.component.html',
+  styleUrl: './star-background.component.scss'
+})
+export class StarBackgroundComponent {
+
+}
