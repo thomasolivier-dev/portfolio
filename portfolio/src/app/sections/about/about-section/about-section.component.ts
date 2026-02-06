92e16d61cc3865804500a2fa90db24e9bf8e856d
thomasolivier-dev <thomas.olivier.pro25@gmail.com>
2026-02-07 00:38:36 +0100

    feat: add about section

--- portfolio/src/app/sections/about/about-section/about-section.component.ts
+++ portfolio/src/app/sections/about/about-section/about-section.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-about-section',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './about-section.component.html',
   styleUrl: './about-section.component.scss'
 })
 export class AboutSectionComponent {
+  public readonly descriptionText: string = `
+    Développeur full-stack diplômé d’un master en génie logiciel, je suis spécialisé en Java, Angular et en gestion de bases de données. J’accorde une grande importance à la qualité du code, à la sécurité et à la maintenabilité des applications que je développe.
 
+    J’ai également de bonnes bases en Python, React et en intelligence artificielle, ce qui me permet d’explorer de nouvelles approches et de m’adapter facilement à différents environnements techniques.
+
+    Passionné par le développement logiciel, je cherche constamment à améliorer mes compétences afin de concevoir des applications fiables, performantes et pensées pour évoluer dans le temps.
+  `.trim();
 }
+
